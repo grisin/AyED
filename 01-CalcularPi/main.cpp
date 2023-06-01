24dbf15f9cd5cf0f033254edbe93bb319a0918a0
Milagros Grisel Ubillos Aguaysol <mubillosaguaysol@frba.utn.edu.ar>
2023-06-01 19:53:09 -0300

    Add files via upload

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+const float PI = 3.141592;
+
+int main(void)
+{
+    float ac = 0;
+    float temp;
+    size_t cont = 0;
+
+    std::cout << std::fixed;
+    std::cout << std::setprecision(6);
+
+    while (ac != PI/4)
+    {
+        temp = pow(-1,cont)/((2 * cont) + 1);
+        ac += temp;
+        cont++;
+    }
+    std::cout << "Valor: " << (4 * ac) << ". Se llego en " << cont << " iteraciones." << std::endl;
+    
+
+    return 0;
+}
